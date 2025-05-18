ba41c1e8339a691870c09527dc6d14f6f867ab3c
TecnoKiller <leodrik.unam@gmail.com>
2025-05-18 16:51:01 -0600

    Ejercicio_6.c

--- Ejercicio_6.c
+++ Ejercicio_6.c
@@ -1,6 +1,6 @@
 /*
 Escriba un programa que pida al usuario un número, y muestre el mensaje "has
-introducido el numero ---" (mostrando el número que ha sido introducido). Esta acción
+introducido el numero" (mostrando el número que ha sido introducido). Esta acción
 debe repetirse hasta que el usuario introduzca el número 0. En ese momento se
 mostrará el mensaje "Finalizando: Se ha introducido el número 0". Además, se debe
 mostrar cuántos números se han introducido y su suma.
