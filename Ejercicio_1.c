e0b9795c87a9ef64a2465cfd2b3a3e69eca9b8c3
TecnoKiller <leodrik.unam@gmail.com>
2025-05-18 16:47:46 -0600

    Ejercicio_1.c

--- Ejercicio_1.c
+++ Ejercicio_1.c
@@ -1,9 +1,11 @@
 //Escriba un programa en C que muestre en pantalla los numeros enteros del 1 al 100
 #include <stdio.h>
 
-int main() {
+int main() 
+{
    
-    for (int i = 1; i <= 100; i++) {
+    for (int i = 1; i <= 100; i++) 
+    {
         printf("%d\n", i); 
     }
 
